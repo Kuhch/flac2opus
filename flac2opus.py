b14ab5db681cdb8008d4677c616975bb7a960d0d
Kuhch <110444574+Kuhch@users.noreply.github.com>
2025-02-06 13:50:54 +0800

    Update flac2opus.py
    Improve parallel performence, redundancy

--- flac2opus.py
+++ flac2opus.py
@@ -4,10 +4,12 @@ from tqdm import tqdm
 import logging
 import tempfile
 from multiprocessing import Pool
-from functools import partial
 
 
-logging.basicConfig(filename=r"D:\flac2opus.log", filemode="a", encoding='utf-8', format="%(asctime)s [%(levelname)s]: %(message)s", datefmt="%Y/%m/%d %H:%M:%S", level=logging.WARNING)
+TARGET_DIR = r"D:\[170404~250119]我喜欢的音乐"
+OUTPUT_DIR = r"D:\[170404~250119]我喜欢的音乐\opus"
+TEMP_DIR = r"R:\tmp"
+logging.basicConfig(filename=r"R:\flac2opus.log", filemode="a", encoding='utf-8', format="%(asctime)s [%(levelname)s]: %(message)s", datefmt="%Y/%m/%d %H:%M:%S", level=logging.WARNING)
 
 
 def detect_format(file):
@@ -23,41 +25,37 @@ def detect_format(file):
 
     return None
 
-def flac2wav(flac, wav):
-    """将flac转换为wav 提取元数据和封面"""
+def flac2meta(flac):
+    """提取flac的元数据和封面"""
 
-    cover = os.path.splitext(wav)[0] + "_cover"
+    name = os.path.splitext(os.path.basename(flac))[0]
+
+    cover = os.path.join(TEMP_DIR, f"{name}_cover")
     result = subprocess.run([
         "ffmpeg", "-i", flac, "-map", "0:v", "-f", "image2", cover
         ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=False)
     if result.returncode != 0:
         cover = None
 
-    metadata = os.path.splitext(wav)[0] + "_metadata.txt"
+    metadata = os.path.join(TEMP_DIR, f"{name}_metadata.txt")
     subprocess.run([
         "ffmpeg", "-i", flac, "-f", "ffmetadata", metadata
         ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
 
-    # stderr=subprocess.DEVNULL
-    subprocess.run([
-        "ffmpeg", "-i", flac, wav
-        ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
-
     return cover, metadata
 
-def resample(wav):
+def resample(flac):
     """sox高品质重采样"""
 
     vol = 0.0
     clip_err = ""
-    name = os.path.splitext(os.path.basename(wav))[0] # 输入文件名（不带扩展名）
-    directory = os.path.dirname(wav)
-    temp_file = os.path.join(directory, f"{name}_48khz.wav")
-    temp_dir = tempfile.mkdtemp(dir=r"D:\tmp") # 创建进程专用的临时目录
+    name = os.path.splitext(os.path.basename(flac))[0] # 输入文件名（不带扩展名）
+    wav = os.path.join(TEMP_DIR, f"{name}.wav")
+    temp_dir = tempfile.mkdtemp(dir=TEMP_DIR) # 创建进程专用的临时目录
 
     try:
         result = subprocess.run([
-            "sox_ng", "--temp", temp_dir, "--multi-threaded", "-G", wav, "-b", "16", temp_file, "rate", "-v", "-I", "-b", "97","48000", "dither", "-s"
+            "sox_ng", "--temp", temp_dir, "--multi-threaded", "-G", flac, "-b", "16", wav, "rate", "-v", "-I", "-b", "97","48000", "dither", "-s"
             ], stderr=subprocess.PIPE, text=True, check=True)
         
         if result.stderr and not "decrease volume?" in result.stderr:
@@ -73,30 +71,26 @@ def resample(wav):
                 break
 
             logging.warning("'" + name + "' -> " + result.stderr + " vol = " + f"{vol:.1f}dB")
-
-            os.remove(temp_file)
             
             result = subprocess.run([
-                "sox_ng", "--temp", temp_dir, "--multi-threaded", "-G", wav, "-b", "16", temp_file, "vol", f"{vol:.1f}dB", "rate", "-v", "-I", "-b", "97","48000", "dither", "-s"
+                "sox_ng", "--temp", temp_dir, "--multi-threaded", "-G", flac, "-b", "16", wav, "vol", f"{vol:.1f}dB", "rate", "-v", "-I", "-b", "97","48000", "dither", "-s"
                 ], stderr=subprocess.PIPE, text=True, check=True)
     finally:
         for filename in os.listdir(temp_dir):
             try:
                 os.remove(os.path.join(temp_dir, filename))
-            except: pass
+            except: pass # 无视清理错误
         try:
             os.rmdir(temp_dir)
         except: pass
 
-    os.replace(temp_file, os.path.join(directory, f"{name}.wav"))
     return vol, clip_err
 
 def wav2opus(wav, cover=None, metadata=None, vol=0.0, clip_err=""):
     """将wav转换为opus 嵌入元数据和封面"""
 
     name = os.path.splitext(os.path.basename(wav))[0]
-    directory = os.path.dirname(wav)
-    opus = os.path.join(directory, f"{name}.opus")
+    opus = os.path.join(OUTPUT_DIR, f"{name}.opus")
 
     comments = []
     dict = {
@@ -133,41 +127,39 @@ def wav2opus(wav, cover=None, metadata=None, vol=0.0, clip_err=""):
 
     subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
 
+    # 清理临时文件
     os.remove(wav)
     if cover:
         os.remove(cover)
     if metadata:
         os.remove(metadata)
 
-def convert(file, path):
+def convert(flac):
     """处理单个文件的工作函数"""
 
     try:
-        file_path = os.path.join(path, file)
-        output_dir = os.path.join(path, "wav")
-        name = os.path.splitext(os.path.basename(file_path))[0]
-        temp_file = os.path.join(output_dir, f"{name}.wav")
+        name = os.path.splitext(os.path.basename(flac))[0]
+        wav = os.path.join(TEMP_DIR, f"{name}.wav")
 
-        cover, metadata = flac2wav(file_path, temp_file)
-        vol, clip_err = resample(temp_file)
-        wav2opus(temp_file, cover, metadata, vol, clip_err)
+        cover, metadata = flac2meta(flac)
+        vol, clip_err = resample(flac)
+        wav2opus(wav, cover, metadata, vol, clip_err)
     except Exception as e:
-        logging.error(f"converting '{file}' Failed: {str(e)}")
+        logging.error(f"converting '{name}' failed: {str(e)}")
         raise
 
+
 if __name__ == "__main__":
-    path = r"D:\[170404~250119]我喜欢的音乐"
-    output_dir = os.path.join(path, "wav")
-    os.makedirs(output_dir, exist_ok=True)
+    path = TARGET_DIR
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
 
     # 创建任务列表
     files = []
-    for file in os.listdir(path):
-        file_path = os.path.join(path, file)
-        if os.path.isfile(file_path) and detect_format(file_path) == "flac":
+    for filename in os.listdir(path):
+        file = os.path.join(path, filename)
+        if os.path.isfile(file) and detect_format(file) == "flac":
             files.append(file)
 
     # 创建进程池并行处理
-    with Pool(processes=2) as pool:
-        processor = partial(convert, path=path)
-        list(tqdm(pool.imap(processor, files), total=len(files)))
+    with Pool(processes=6) as pool:
+        list(tqdm(pool.imap_unordered(convert, files), total=len(files)))
